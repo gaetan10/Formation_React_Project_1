b874a8f01c0534297455ad7b0d904e8cd162d888
gaetan <gaetan.may@gmail.com>
2024-01-09 19:31:01 -0500

    Initial commit

---
+++ src/components/TabButton.jsx
@@ -0,0 +1,11 @@
+import { Children } from "react";
+
+export default function TabButton({ children, isSelected, ...props }) {
+  return (
+    <li>
+      <button className={isSelected ? "active" : undefined} {...props}>
+        {children}
+      </button>
+    </li>
+  );
+}
